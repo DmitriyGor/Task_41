1a65436617881dd4479bac665df9e84df906b2d9
Dmitriy <79133850099@yandex.ru>
2024-01-04 02:19:41 +0700

    Task 41

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Задача 41: 
+Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+/* Ввод элементов через пробел
+Console.Write("Введите элементы (через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+*/
+Console.Write("Введите число -> размер массива: ");
+int massive = Convert.ToInt32(Console.ReadLine());
+int[]arr = new int[massive];
+for (int i = 0; i < massive; i++)
+{
+    Console.Clear();
+    Console.Write("Введите целое число массива (положительное или отрицательное):  ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    arr[i] = number;
+    
+}
+Console.Clear();
+Console.WriteLine($"Массив: [ {String.Join("; ",arr)} ]");
+int positive = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+       positive += 1;
+    }
+}
+Console.WriteLine($"Количество элементов в массиве больше нуля :  {positive} ");
+
